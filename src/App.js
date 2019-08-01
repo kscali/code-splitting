4e13b88f76b84dbc82f09c23691f0f96c1617cfb
koy <kosatun@aol.com>
2019-08-01 11:45:24 -0700

    created components and ayncCompenent

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import Page1 from './components/Page1';
+// import Page2 from './components/Page2';
+// import Page3 from './components/Page3';
+import AsyncComponent from './components/AsyncCompnent';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: 'page1',
+      component: null
+    };
+  }
+
+  onRouteChange = route => {
+    this.setState({ route });
+  };
+
+  render() {
+    if (this.state.route === 'page1') {
+      return <Page1 onRouteChange={this.onRouteChange} />;
+    } else if (this.state.route === 'page2') {
+      const AsyncPage2 = AsyncComponent(() => import('./components/Page2'));
+      return <AsyncPage2 onRouteChange={this.onRouteChange} />;
+    } else if (this.state.route === 'page3') {
+      const AsyncPage3 = AsyncComponent(() => import('./components/Page3'));
+      return <AsyncPage3 onRouteChange={this.onRouteChange} />;
+    }
+
+    // if (this.state.route === 'page1') {
+    //   return <Page1 onRouteChange={this.onRouteChange} />;
+    // } else {
+    //   return <this.state.component onRouteChange={this.onRouteChange} />;
+    // }
+  }
 }
 
 export default App;
